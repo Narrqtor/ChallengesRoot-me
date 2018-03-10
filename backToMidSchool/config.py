459c36ceec6b409634a7734c97ab2f873749a3cc
Nathanaël BELVO <nathanael.belvo@gmail.com>
2018-03-11 03:51:58 +1100

    added config file

---
+++ backToMidSchool/config.py
@@ -0,0 +1,13 @@
+import string
+
+NONE = ""
+PASSWORD = NONE
+NICKNAME = "Narrqtor"
+REAL_NAME = "Nathanaël"
+DOMAIN = "mowgli"
+SERVER = "irc.root-me.org"
+CHANNEL = "#root-me_challenge"
+PONG = "PONG"
+QUITMSG = "Goodbye"
+ERRORMSG = "Critical error"
+
